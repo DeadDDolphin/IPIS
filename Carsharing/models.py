eb414ab83034ee801c16c4cdb2ca2a3d146bc51b
DeadDDolphin <62338390+DeadDDolphin@users.noreply.github.com>
2021-03-12 04:22:50 +0300

    Update models.py
    Добавил функции добавления в модель

--- Carsharing/models.py
+++ Carsharing/models.py
@@ -1,5 +1,5 @@
 from peewee import *
-from connectToDB import BaseModel
+from connectToDB import *
 
 class Carsharing(BaseModel):
     birth_date = DateField(column_name='BirthDate')
@@ -8,6 +8,7 @@ class Carsharing(BaseModel):
     driver_license_number = CharField(column_name='DriverLicenseNumber')
     driving_experience = IntegerField(column_name='DrivingExperience')
     id_client = AutoField(column_name='ID_Client')
+    gender = CharField(column_name="Gender")
     name = CharField(column_name='Name')
     order_date = DateField(column_name='OrderDate', null=True)
     phone_number = CharField(column_name='PhoneNumber')
@@ -16,6 +17,18 @@ class Carsharing(BaseModel):
     tariff = CharField(column_name='Tariff')
     trip_end_time = DateField(column_name='TripEndTime', null=True)
     trip_start_time = DateField(column_name='TripStartTime', null=True)
+
+    #Функция для добавления ОДНОГО элемента в модель и в базу данных
+    #На вход подается словарь вида {"field_name":"field_value"}
+    def insertOne(self, data):
+        self.create(data)
+
+    #Функция для добавления МНОГИХ элементов в модель и в базу данных
+    #На вход подается словарь вида {"field_name":"field_value"}
+    def insertMany(self, data):
+        with database.atomic():
+            for every_row in data:
+                self.create(**every_row)
 
     class Meta:
         table_name = 'carsharing'
